2bf5c597ae2f0371739b866ab7356b477ae9d518
solut <https://github.com/ttpoh/ttpoh.git>
2022-05-13 15:48:29 +0900

    server

---
+++ server/conn.php
@@ -0,0 +1,13 @@
+<?php
+$db_name = "solution12441"; 		// DB 명
+ $username = "solution12441";		// DB 아이디
+ $password = "z11626815!"; 		// MySQL 비밀번호
+ $servername = "localhost";
+
+ $con = mysqli_connect($servername, $username, $password, $db_name);
+
+//  if($conn){
+//   echo "Login success";}
+//   else{
+//   echo "Login fail";}
+?>
